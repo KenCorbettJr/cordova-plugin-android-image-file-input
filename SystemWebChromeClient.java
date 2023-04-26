ac0fc55948bd58b21508652774dec6adc891faa7
Ken Corbett <ken@truepic.com>
2023-04-26 10:13:43 -0400

    A little cleanup

--- SystemWebChromeClient.java
+++ SystemWebChromeClient.java
@@ -1,4 +1,3 @@
-
 /*
 In order to get the file input to work correctly and allow you to add pictures from your
 camera, you need this updated SystemWebChromeClient. It is a modified version of the
@@ -8,27 +7,8 @@ an updated onFileChooser method that allows you to add pictures from your camera
 This plugin automatically copies this file to the right location.  Essentially:
 
 cp SystemWebChromeClient.java platforms/android/CordovaLib/src/org/apache/cordova/engine
-
 */
 
-/*
-       Licensed to the Apache Software Foundation (ASF) under one
-       or more contributor license agreements.  See the NOTICE file
-       distributed with this work for additional information
-       regarding copyright ownership.  The ASF licenses this file
-       to you under the Apache License, Version 2.0 (the
-       "License"); you may not use this file except in compliance
-       with the License.  You may obtain a copy of the License at
-
-         http://www.apache.org/licenses/LICENSE-2.0
-
-       Unless required by applicable law or agreed to in writing,
-       software distributed under the License is distributed on an
-       "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-       KIND, either express or implied.  See the License for the
-       specific language governing permissions and limitations
-       under the License.
-*/
 package org.apache.cordova.engine;
 
 import java.io.IOException;
